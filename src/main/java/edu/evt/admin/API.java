cc7a3c51ae03712a86cf8d13f00656335212db88
Wheeler Law <whelderwheels613@gmail.com>
2016-09-08 19:48:40 -0400

    Initial commit with beginnings of API

---
+++ src/main/java/edu/evt/admin/API.java
@@ -0,0 +1,10 @@
+package edu.evt.admin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class API{
+	public static void main(String[] args){
+		SpringApplication.run(API.class, args);
+	}
+}
