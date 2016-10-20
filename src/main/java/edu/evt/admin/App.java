e90b1fe99353c222625ac8b3d877fee217b8b329
Wheeler Law <whelderwheels613@gmail.com>
2016-10-20 15:15:03 -0400

    Figured out how to serve static content as well as running the API.

--- src/main/java/edu/evt/admin/App.java
+++ src/main/java/edu/evt/admin/App.java
@@ -1,19 +1,56 @@
 package edu.evt.admin;
 
-import javax.ws.rs.core.Application;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import edu.evt.admin.resource.Calculator;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
 
-/**
- * Created by Wheeler on 10/6/2016.
- */
-public class App extends Application {
-    @Override
-    public Set<Class<?>> getClasses(){
-        Set<Class<?>> classes = new HashSet<>();
-        classes.add(Calculator.class);
-        return Collections.unmodifiableSet(classes);
-    }
+import java.util.*;
+
+public class App extends ResourceConfig{
+
+	/**
+	 * When the App is run as a servlet.
+	 */
+	public App(){
+		packages("edu.evt.admin.resource");
+	}
+
+	/**
+	 * This app can be run within it's own context in addition to running as a servlet in a container.
+	 * @param args
+	 * @throws Exception
+     */
+	public static void main(String[] args) throws Exception{
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+
+		Server server = new Server(8080);
+		server.setHandler(context);
+
+		ServletHolder apiHolder = new ServletHolder("api", new ServletContainer(new App()));
+		ServletHolder staticHolder = new ServletHolder("static", new DefaultServlet());
+		staticHolder.setInitParameter("resourceBase", "./src/ui");
+
+		context.addServlet(apiHolder, "/api/*");
+		context.addServlet(staticHolder, "/");
+
+//		ServletHolder servlet = context.addServlet(ServletContainer.class, "/*");
+//		servlet.setInitOrder(0);
+//
+//		Map<String, String> servletParams = new HashMap<>();
+//		servletParams.put("javax.ws.rs.Application", App.class.getCanonicalName());
+//		servlet.setInitParameters(servletParams);
+
+		try {
+			server.start();
+			server.join();
+		}finally{
+			server.destroy();
+		}
+
+	}
+
 }
