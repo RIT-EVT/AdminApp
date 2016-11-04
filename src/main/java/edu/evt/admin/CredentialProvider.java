a37c146c0efae54cb52d5c95758d59a5bfae2c49
Wheeler Law <whelderwheels613@gmail.com>
2016-11-04 17:19:57 -0400

    Wrote some more things for the credentials provider

--- src/main/java/edu/evt/admin/CredentialProvider.java
+++ src/main/java/edu/evt/admin/CredentialProvider.java
@@ -3,6 +3,10 @@ package edu.evt.admin;
 
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.mongodb.client.model.Filters.*;
 
 /**
@@ -17,8 +21,17 @@ public class CredentialProvider {
 
 	public String get(String key){
         MongoCollection<Document> credentials = App.db.getCollection("credentials");
-        Document doc = credentials.find(eq("key", "wiki_password")).first();
+        Document doc = credentials.find(eq("key", key)).first();
 
-        return doc.toJson();
+        return doc.getString("value");
 	}
+
+	public void store(String key, String value){
+        MongoCollection<Document> credentials = App.db.getCollection("credentials");
+        Map<String, Object> doc = new HashMap<String, Object>(){{
+            put("key", key);
+            put("value", value);
+        }};
+        credentials.insertOne(new Document(doc));
+    }
 }
