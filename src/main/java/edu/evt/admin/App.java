7da5245717053e1d6a03abd8a319d18add8066a1
Wheeler Law <whelderwheels613@gmail.com>
2016-11-04 15:55:23 -0400

    Started work on the CredentialProvider

--- src/main/java/edu/evt/admin/App.java
+++ src/main/java/edu/evt/admin/App.java
@@ -1,28 +1,41 @@
 package edu.evt.admin;
 
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
+import org.jongo.Jongo;
 
-import java.util.*;
 
 public class App extends ResourceConfig{
+
+    public static MongoClient mongo;
+    public static MongoDatabase db;
+
+    public static CredentialProvider credentialProvider;
 
 	/**
 	 * When the App is run as a servlet.
 	 */
 	public App(){
 		packages("edu.evt.admin.resource");
+        App.mongo = new MongoClient();
+        App.db = App.mongo.getDatabase("admin");
+
+        App.credentialProvider = new CredentialProvider();
+
 	}
 
 	/**
 	 * This app can be run within it's own context in addition to running as a servlet in a container.
 	 * @param args
 	 * @throws Exception
-     */
+	 */
 	public static void main(String[] args) throws Exception{
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
--- src/main/java/edu/evt/admin/CredentialProvider.java
+++ src/main/java/edu/evt/admin/CredentialProvider.java
@@ -1,8 +1,24 @@
 package edu.evt.admin;
 
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import static com.mongodb.client.model.Filters.*;
+
 /**
  * Created by wheeler on 11/3/16.
  */
 public class CredentialProvider {
-    // TODO: Implement this!
+	// TODO: Implement this!
+
+    public CredentialProvider(){
+        App.db.createCollection("credentials");
+    }
+
+	public String get(String key){
+        MongoCollection<Document> credentials = App.db.getCollection("credentials");
+        Document doc = credentials.find(eq("key", "wiki_password")).first();
+
+        return doc.toJson();
+	}
 }
