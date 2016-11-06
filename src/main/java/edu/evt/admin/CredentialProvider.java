9b1f8b71196c504876ac9f678b26e9f4845e1253
Wheeler Law <whelderwheels613@gmail.com>
2016-11-06 01:36:28 -0400

    Added a setup task to the build script

--- src/main/java/edu/evt/admin/CredentialProvider.java
+++ src/main/java/edu/evt/admin/CredentialProvider.java
@@ -16,7 +16,6 @@ public class CredentialProvider {
 	// TODO: Implement this!
 
     public CredentialProvider(){
-        App.db.createCollection("credentials");
     }
 
 	public String get(String key){
@@ -26,6 +25,7 @@ public class CredentialProvider {
         return doc.getString("value");
 	}
 
+	// TODO: Eventually need to add some sort of encryption for storing the passwords in the database.
 	public void store(String key, String value){
         MongoCollection<Document> credentials = App.db.getCollection("credentials");
         Map<String, Object> doc = new HashMap<String, Object>(){{
